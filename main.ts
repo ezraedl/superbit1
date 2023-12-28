f35da60ed471a59700ad69d7ab5bebc6dd5b0ca2
ezraedl <50549215+ezraedl@users.noreply.github.com>
2023-12-28 21:26:43 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,61 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (0 == receivedNumber) {
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        255,
+        SuperBit.enMotors.M2,
+        255
+        )
+    }
+    if (1 == receivedNumber) {
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        -255,
+        SuperBit.enMotors.M2,
+        -255
+        )
+    }
+    if (2 == receivedNumber) {
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        0,
+        SuperBit.enMotors.M2,
+        0
+        )
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendNumber(2)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    radio.sendNumber(0)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(2)
+})
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . # #
+        . . # . .
+        . . . # #
+        `)
+    basic.showLeds(`
+        . . # # .
+        . # . . #
+        . . . . #
+        . . # # #
+        . # . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        . . # . .
+        `)
 })
